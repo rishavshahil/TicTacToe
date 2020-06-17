e5bf045f5557081b2b2bdd51620e61094cbba3c4
rishavshahil <67056512+rishavshahil@users.noreply.github.com>
2020-06-18 02:05:17 +0530

    Update Tic tec to.c

--- Tic tec to.c
+++ Tic tec to.c
@@ -1,34 +1,34 @@
 #include<stdio.h>
 #include<conio.h>
 void Print_Value(char*);
+void Print_Title()
+{
+    printf(" #######################################\n");
+    printf(" ####  WELCOME TO TIC TAC TOE GAME  ####\n");
+    printf(" ####     [Developed by Rishav]     ####\n");
+    printf(" #######################################\n\n");
+
+}
 int Input1(char *A,int choice)
 {
    if(choice>9){
     printf("               Invalid Index.\n               Please Input Again...");
     getch();
     system("cls");
-    printf(" #######################################\n");
-    printf(" ####                               ####\n");
-    printf(" ####  WELCOME TO TIC TAC TOE GAME  ####\n");
-    printf(" ####                               ####\n");
-    printf(" #######################################\n\n");
+    Print_Title();
     Print_Value(A);
     return(1);
    }
-   else if(A[choice-1]=='0'||A[choice-1]=='X'){
+   else if(A[choice-1]=='O'||A[choice-1]=='X'){
             printf("               Same Index.\n               Please Try Again with Different Index");
             getch();
             system("cls");
-            printf(" #######################################\n");
-            printf(" ####                               ####\n");
-            printf(" ####  WELCOME TO TIC TAC TOE GAME  ####\n");
-            printf(" ####                               ####\n");
-            printf(" #######################################\n\n");
+            Print_Title();
             Print_Value(A);
             return(1);
         }
         else
-            A[choice-1]='0';
+            A[choice-1]='O';
             return(0);
 }
 int Input2(char *A,int choice)
@@ -37,23 +37,15 @@ int Input2(char *A,int choice)
         printf("               Invalid Index.\n               Please Input Again...");
         getch();
         system("cls");
-        printf(" #######################################\n");
-        printf(" ####                               ####\n");
-        printf(" ####  WELCOME TO TIC TAC TOE GAME  ####\n");
-        printf(" ####                               ####\n");
-        printf(" #######################################\n\n");
+        Print_Title();
         Print_Value(A);
         return(1);
     }
-   else if(A[choice-1]=='X'||A[choice-1]=='0'){
+   else if(A[choice-1]=='X'||A[choice-1]=='O'){
             printf("               Same Index.\n               Please Try Again with Different Index");
             getch();
             system("cls");
-            printf(" #######################################\n");
-            printf(" ####                               ####\n");
-            printf(" ####  WELCOME TO TIC TAC TOE GAME  ####\n");
-            printf(" ####                               ####\n");
-            printf(" #######################################\n\n");
+            Print_Title();
             Print_Value(A);
             return(1);
         }
@@ -63,21 +55,21 @@ int Input2(char *A,int choice)
 }
 int Win1(char *A)
 {
-    if(A[0]==A[1]&&A[1]==A[2]&&A[0]=='0')
+    if(A[0]==A[1]&&A[1]==A[2]&&A[0]=='O')
         return(1);
-    else if(A[3]==A[4]&&A[4]==A[5]&&A[3]=='0')
+    else if(A[3]==A[4]&&A[4]==A[5]&&A[3]=='O')
             return(1);
-    else if(A[6]==A[7]&&A[7]==A[8]&&A[6]=='0')
+    else if(A[6]==A[7]&&A[7]==A[8]&&A[6]=='O')
             return(1);
-    else if(A[0]==A[3]&&A[3]==A[6]&&A[0]=='0')
+    else if(A[0]==A[3]&&A[3]==A[6]&&A[0]=='O')
             return(1);
-    else if(A[1]==A[4]&&A[4]==A[7]&&A[1]=='0')
+    else if(A[1]==A[4]&&A[4]==A[7]&&A[1]=='O')
             return(1);
-    else if(A[2]==A[5]&&A[5]==A[8]&&A[2]=='0')
+    else if(A[2]==A[5]&&A[5]==A[8]&&A[2]=='O')
             return(1);
-    else if(A[0]==A[4]&&A[4]==A[8]&&A[0]=='0')
+    else if(A[0]==A[4]&&A[4]==A[8]&&A[0]=='O')
             return(1);
-    else if(A[2]==A[4]&&A[4]==A[6]&&A[2]=='0')
+    else if(A[2]==A[4]&&A[4]==A[6]&&A[2]=='O')
             return(1);
     else
         return(0);
@@ -108,7 +100,7 @@ int Draw(char *A)
     int i;
     for(i=0;i<9;i++)
         if(A[i]!='X')
-            if(A[i]!='0')
+            if(A[i]!='O')
                 return(0);
     return(1);
 }
@@ -120,6 +112,13 @@ int Exit()
     printf("\n\n            Enter Your Choice: ");
     scanf("%d",&choice);
     return(choice);
+}
+void Set_value(char *A)
+{
+  int i;
+  char j;
+  for(i=0,j=49;i<=8;i++,j++)
+    A[i]=j;
 }
 void Print_Value(char *A)
 {
@@ -139,83 +138,79 @@ void Print_Value(char *A)
 }
 main()
 {
-    int choice1,choice2,x1,x2,x3;
-    char A[9]={'1','2','3','4','5','6','7','8','9'},P1[15],P2[15];
-    printf(" #######################################\n");
-    printf(" ####                               ####\n");
-    printf(" ####  WELCOME TO TIC TAC TOE GAME  ####\n");
-    printf(" ####                               ####\n");
-    printf(" #######################################\n\n");
-    printf("       Enter 1st Player Name: ");
-    gets(P1);
-    printf("       Enter 2nd Player Name: ");
-    gets(P2);
-    printf("\n       Hi %s And %s.",P1,P2);
-    printf("\n       Press Enter To Start The GAME...");
-    getch();
     while(1){
-        while(1){
-            x1=x2=1;
-            choice1=choice2=10;
-            system("cls");
-            printf(" #######################################\n");
-            printf(" ####                               ####\n");
-            printf(" ####  WELCOME TO TIC TAC TOE GAME  ####\n");
-            printf(" ####                               ####\n");
-            printf(" #######################################\n\n");
-            Print_Value(A);
-            while(x1){
-                printf("\n\n               %s[0]: ",P1);
-                scanf("%d",&choice1);
-                x1=Input1(A,choice1);
-            }
-            system("cls");
-            printf(" #######################################\n");
-            printf(" ####                               ####\n");
-            printf(" ####  WELCOME TO TIC TAC TOE GAME  ####\n");
-            printf(" ####                               ####\n");
-            printf(" #######################################\n\n");
-            Print_Value(A);
-            if(Win1(A)==1||Win2(A)==1||Draw(A)==1)
-                break;
-            while(x2){
-                printf("\n\n               %s[X]: ",P2);
-                scanf("%d",&choice2);
-                x2=Input2(A,choice2);
-            }
-            if(Win1(A)==1||Win2(A)==1||Draw(A)==1)
-                break;
-        }
-        if(Draw(A)==1)
-            printf("\n\n               Match Draw");
-        else if(Win1(A)==1)
-            printf("\n         Congo %s[0] You Win.",P1);
-        else
-            printf("\n         Congo %s[X] You Win.",P2);
+        int choice1,choice2,x1,x2,x3,x4;
+        char A[9],P1[15],P2[15];
+        system("cls");
+        fflush(stdin);
+        Print_Title();
+        printf("       Enter 1st Player Name: ");
+        gets(P1);
+        printf("       Enter 2nd Player Name: ");
+        gets(P2);
+        printf("\n       Hi %s And %s.",P1,P2);
+        printf("\n       Press Enter To Start The GAME...");
         getch();
-        x3=3;
-        while(x3!=1){
+        while(1){
+            Set_value(A);
+            while(1){
+                x1=x2=1;
+                choice1=choice2=10;
+                system("cls");
+                Print_Title();
+                Print_Value(A);
+                while(x1){
+                    printf("\n\n               %s[O]: ",P1);
+                    scanf("%d",&choice1);
+                    x1=Input1(A,choice1);
+                }
+                system("cls");
+                Print_Title();
+                Print_Value(A);
+                if(Win1(A)==1||Win2(A)==1||Draw(A)==1)
+                    break;
+                while(x2){
+                    printf("\n\n               %s[X]: ",P2);
+                    scanf("%d",&choice2);
+                    x2=Input2(A,choice2);
+                }
+                if(Win1(A)==1||Win2(A)==1||Draw(A)==1)
+                    break;
+            }
+            if(Draw(A)==1)
+                printf("\n\n               Match Draw");
+            else if(Win1(A)==1)
+                printf("\n\n           Congratulation %s[O]\n                   You Win.",P1);
+            else
+                printf("\n           Congratulation %s[X]\n                   You Win.",P2);
+            getch();
+            x3=3;
+            while(x3!=1){
+                system("cls");
+                Print_Title();
+                x3=Exit();
+                switch(x3)
+                {
+                case 1:
+                    break;
+                case 2:
+                    printf("\n            Thank You.\n            Press Any Key to Exit.");
+                    getch();
+                    exit(0);
+                default:
+                    printf("            Invalid choice,Please Try Again.");
+                    getch();
+                    break;
+                }
+            }
             system("cls");
-            printf(" #######################################\n");
-            printf(" ####                               ####\n");
-            printf(" ####  WELCOME TO TIC TAC TOE GAME  ####\n");
-            printf(" ####                               ####\n");
-            printf(" #######################################\n\n");
-            x3=Exit();
-            switch(x3)
-            {
-            case 1:
+            Print_Title();
+            printf("\n         1.Continue With same Player.");
+            printf("\n         2.Play With Another.");
+            printf("\n\n         Enter your Choice: ");
+            scanf("%d",&x4);
+            if(x4!=1)
                 break;
-            case 2:
-                printf("            \nThank You.\n            Press Any Key to Exit.");
-                getch();
-                exit(0);
-            default:
-                printf("            Invalid choice,Please Try Again.");
-                getch();
-            break;
-
-        }
         }
     }
 }
